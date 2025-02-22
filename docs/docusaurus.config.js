4c01f73593041e3b3ad8507fc5b7f9ef383f28c0
Nuno do Carmo <nuno.carmo@suse.com>
2025-02-22 11:35:57 +0100

    Updated baseUrl
    For the live website, the baseUrl needs to be set to "/" instead of "/devex-designs"
    
    Signed-off-by: Nuno do Carmo nuno.carmo@suse.com

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://rancherlabs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/devex-designs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
